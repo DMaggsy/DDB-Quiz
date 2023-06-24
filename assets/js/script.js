4506caabc162230bf3fe21ec67d12ee024a06153
Danny Maggs <131584189+DMaggsy@users.noreply.github.com>
2023-06-24 12:12:52 +0100

    Move script.js to js folder

---
+++ assets/js/script.js
@@ -0,0 +1,111 @@
+const questions = {
+    "categories": [{
+            "name": "Wildlife",
+            "questions": [{
+                    "question": "What is the fastest type of shark?",
+                    "options": ["Great White", "Hammerhead", "Mako", "Tiger"],
+                    "answer": "Mako"
+                },
+                {
+                    "question": "What is the oldest estimated age of a Greenland shark?",
+                    "options": ["100", "200", "300", "400"],
+                    "answer": "400"
+                },
+                {
+                    "question": "Which animal has the largest brain in ratio to its body size?",
+                    "options": ["Dolphin", "Elephant", "Human", "Sperm Whale"],
+                    "answer": "Sperm Whale"
+                },
+                {
+                    "question": "Which animal is known for its exceptional memory?",
+                    "options": ["Elephant", "Octopus", "Dog", "Chimpanzee"],
+                    "answer": "Elephant"
+                },
+                {
+                    "question": "Which animal can hold its breath the longest underwater?",
+                    "options": ["Hippo", "Seal", "Turtle", "Whale"],
+                    "answer": "Whale"
+                },
+                {
+                    "question": "Which animal is the largest mammal on Earth?",
+                    "options": ["Elephant", "Giraffe", "Blue Whale", "Gorilla"],
+                    "answer": "Blue Whale"
+                },
+                {
+                    "question": "Which bird has the largest wingspan?",
+                    "options": ["Albatross", "Condor", "Eagle", "Pelican"],
+                    "answer": "Albatross"
+                },
+                {
+                    "question": "Which bird can fly backward?",
+                    "options": ["Hummingbird", "Ostrich", "Penguin", "Kiwi"],
+                    "answer": "Hummingbird"
+                },
+                {
+                    "question": "Which bird is the symbol of wisdom?",
+                    "options": ["Owl", "Eagle", "Peacock", "Swan"],
+                    "answer": "Owl"
+                },
+                {
+                    "question": "Which bird is known for its ability to mimic human speech?",
+                    "options": ["Parrot", "Flamingo", "Sparrow", "Crow"],
+                    "answer": "Parrot"
+                }
+            ]
+        },
+        {
+            "name": "Motorbikes",
+            "questions": [{
+                    "question": "What is the top speed of the Kawasaki Ninja H2R?",
+                    "options": ["200 km/h", "300 km/h", "400 km/h", "500 km/h"],
+                    "answer": "400 km/h"
+                },
+                {
+                    "question": "Which motorcycle brand holds the record for the most Grand Prix Motorcycle Racing World Championships?",
+                    "options": ["Honda", "Yamaha", "Ducati", "Suzuki"],
+                    "answer": "Honda"
+                },
+                {
+                    "question": "What is the engine displacement of the Harley-Davidson Fat Boy?",
+                    "options": ["750cc", "1200cc", "1600cc", "1800cc"],
+                    "answer": "1800cc"
+                },
+                {
+                    "question": "Which motorcycle company introduced the first mass-produced electric motorcycle?",
+                    "options": ["Zero Motorcycles", "Harley-Davidson", "BMW Motorrad", "Ducati"],
+                    "answer": "Zero Motorcycles"
+                },
+                {
+                    "question": "What is the purpose of a catalytic converter in a motorcycle's exhaust system?",
+                    "options": ["Increasing top speed", "Enhancing fuel efficiency", "Reducing emissions", "Improving suspension"],
+                    "answer": "Reducing emissions"
+                },
+                {
+                    "question": "Which motorcycle race is known as 'The Isle of Man TT'?",
+                    "options": ["British Superbike Championship", "MotoGP", "World Superbike Championship", "Tourist Trophy"],
+                    "answer": "Tourist Trophy"
+                },
+                {
+                    "question": "What is the meaning of the abbreviation 'ABS' in relation to motorcycles?",
+                    "options": ["Anti-Brake Skid", "Automatic Braking System", "Advanced Bike Suspension", "Anti-Lock Braking System"],
+                    "answer": "Anti-Lock Braking System"
+                },
+                {
+                    "question": "Which motorcycle brand is famous for its off-road bikes, such as the CRF series?",
+                    "options": ["Suzuki", "KTM", "Kawasaki", "Triumph"],
+                    "answer": "Honda"
+                },
+                {
+                    "question": "What is the purpose of a quickshifter in a motorcycle?",
+                    "options": ["To adjust the suspension settings", "To change gears without using the clutch", "To control the throttle response", "To activate the traction control system"],
+                    "answer": "To change gears without using the clutch"
+                },
+                {
+                    "question": "Which motorcycle company produces the 'Ducati Panigale V4' superbike?",
+                    "options": ["Ducati", "Yamaha", "Kawasaki", "Honda"],
+                    "answer": "Ducati"
+                }
+            ]
+        }
+    ]
+}
