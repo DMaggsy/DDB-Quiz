1f2f9905873be5a4d22c7d4e6ba2a646abcc68b2
Danny Maggs <131584189+DMaggsy@users.noreply.github.com>
2023-06-26 17:38:56 +0100

    Updated js file to fetch questions from json file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,166 +1,151 @@
 document.addEventListener('DOMContentLoaded', function() {
-// Define variables
-var currentCategoryIndex = 0;
-var currentQuestionIndex = 0;
-var score = 0;
-
-// Get the start button element
-var startButton = document.getElementById('start-btn');
-
-// Get the question container elements
-var questionContainer = document.getElementById('question-container');
-var questionElement = document.getElementById('question');
-var optionsElement = document.getElementById('options');
-
-// Get the submit button and result elements
-var submitButton = document.getElementById('submit-btn');
-var resultElement = document.getElementById('result');
-
-// Add event listener to the start button
-startButton.addEventListener('click', startQuiz);
-
-// Function to start the quiz
-function startQuiz() {
-  // Hide the start button and show the question container
-  startButton.style.display = 'none';
-  questionContainer.style.display = 'block';
+    // Define variables
+    var currentCategoryIndex = 0;
+    var currentQuestionIndex = 0;
+    var score = 0;
   
-  // Load the first question
-  loadQuestion();
-}
-
-// Function to load a question
-function loadQuestion() {
-  // Get the current category and question
-  var category = categories[currentCategoryIndex];
-  var question = category.questions[currentQuestionIndex];
+    // Get the start button element
+    var startButton = document.getElementById('start-btn');
   
-  // Set the question and options in the HTML elements
-  questionElement.innerText = question.question;
-  optionsElement.innerHTML = '';
+    // Get the question container elements
+    var questionContainer = document.getElementById('question-container');
+    var questionElement = document.getElementById('question');
+    var optionsElement = document.getElementById('options');
   
-  // Create and append the option buttons
-  for (var i = 0; i < question.options.length; i++) {
-    var optionButton = document.createElement('button');
-    optionButton.innerText = question.options[i];
-    optionButton.classList.add('option-btn');
-    optionButton.addEventListener('click', selectOption);
-    optionsElement.appendChild(optionButton);
-  }
-}
-
-// Function to handle option selection
-function selectOption(event) {
-  var selectedOption = event.target;
-  var category = categories[currentCategoryIndex];
-  var question = category.questions[currentQuestionIndex];
+    // Get the submit button and result elements
+    var submitButton = document.getElementById('submit-btn');
+    var resultElement = document.getElementById('result');
   
-  // Check if the selected option is correct
-  if (selectedOption.innerText === question.answer) {
-    score++;
-  }
+    // Add event listener to the start button
+    startButton.addEventListener('click', startQuiz);
   
-  // Disable option buttons to prevent multiple selections
-  var optionButtons = optionsElement.querySelectorAll('.option-btn');
-  for (var i = 0; i < optionButtons.length; i++) {
-    optionButtons[i].disabled = true;
-  }
+    // Function to start the quiz
+    function startQuiz() {
+      // Hide the start button and show the question container
+      startButton.style.display = 'none';
+      questionContainer.style.display = 'block';
   
-  // Show the submit button
-  submitButton.style.display = 'block';
-}
-
-// Add event listener to the submit button
-submitButton.addEventListener('click', submitAnswer);
-
-// Function to submit the answer and load the next question
-function submitAnswer() {
-  // Increment the question index
-  currentQuestionIndex++;
-  
-  // Check if all questions in the current category are answered
-  var category = categories[currentCategoryIndex];
-  if (currentQuestionIndex >= category.questions.length) {
-    // Check if there are more categories
-    if (currentCategoryIndex + 1 < categories.length) {
-      // Move to the next category
-      currentCategoryIndex++;
-      currentQuestionIndex = 0;
-    } else {
-      // End the quiz and display the result
-      endQuiz();
-      return;
+      // Load the first question
+      loadQuestion();
     }
-  }
   
-  // Load the next question
-  loadQuestion();
+    // Function to load a question
+    function loadQuestion() {
+      // Get the current category and question
+      var category = categories[currentCategoryIndex];
+      var question = category.questions[currentQuestionIndex];
   
-  // Clear the selected option and enable option buttons
-  var optionButtons = optionsElement.querySelectorAll('.option-btn');
-  for (var i = 0; i < optionButtons.length; i++) {
-    optionButtons[i].disabled = false;
-    optionButtons[i].classList.remove('selected');
-  }
+      // Set the question and options in the HTML elements
+      questionElement.innerText = question.question;
+      optionsElement.innerHTML = '';
   
-  // Hide the submit button
-  submitButton.style.display = 'none';
-}
-
-// Function to end the quiz and display the result
-function endQuiz() {
-  // Hide the question container
-  questionContainer.style.display = 'none';
-  
-  // Calculate the score percentage
-  var totalQuestions = 0;
-  var totalCorrect = 0;
-  for (var i = 0; i < categories.length; i++) {
-    totalQuestions += categories[i].questions.length;
-    totalCorrect += getCorrectAnswersCount(categories[i]);
-  }
-  var scorePercentage = (score / totalQuestions) * 100;
-  
-  // Display the result
-  resultElement.innerHTML = 'Quiz ended.<br>';
-  resultElement.innerHTML += 'Your score: ' + score + '/' + totalQuestions + '<br>';
-  resultElement.innerHTML += 'Score percentage: ' + scorePercentage.toFixed(2) + '%';
-}
-
-// Function to count the number of correct answers in a category
-function getCorrectAnswersCount(category) {
-  var correctCount = 0;
-  for (var i = 0; i < category.questions.length; i++) {
-    if (category.questions[i].answer === category.questions[i].selectedOption) {
-      correctCount++;
+      // Create and append the option buttons
+      for (var i = 0; i < question.options.length; i++) {
+        var optionButton = document.createElement('button');
+        optionButton.innerText = question.options[i];
+        optionButton.classList.add('option-btn');
+        optionButton.addEventListener('click', selectOption);
+        optionsElement.appendChild(optionButton);
+      }
     }
-  }
-  return correctCount;
-}
-
-// Load the categories from the JSON file
-var categories = [
-  {
-    "name": "Wildlife",
-    "questions": [
-      {
-        "question": "What is the fastest type of shark?",
-        "options": ["Great White", "Hammerhead", "Mako", "Tiger"],
-        "answer": "Mako"
-      },
-      // Add more questions...
-    ]
-  },
-  {
-    "name": "Motorbikes",
-    "questions": [
-      {
-        "question": "What is the top speed of the Kawasaki Ninja H2R?",
-        "options": ["200 km/h", "300 km/h", "400 km/h", "500 km/h"],
-        "answer": "400 km/h"
-      },
-      // Add more questions...
-    ]
-  }
-];
-});
+  
+    // Function to handle option selection
+    function selectOption(event) {
+      var selectedOption = event.target;
+      var category = categories[currentCategoryIndex];
+      var question = category.questions[currentQuestionIndex];
+  
+      // Check if the selected option is correct
+      if (selectedOption.innerText === question.answer) {
+        score++;
+      }
+  
+      // Disable option buttons to prevent multiple selections
+      var optionButtons = optionsElement.querySelectorAll('.option-btn');
+      for (var i = 0; i < optionButtons.length; i++) {
+        optionButtons[i].disabled = true;
+      }
+  
+      // Show the submit button
+      submitButton.style.display = 'block';
+    }
+  
+    // Add event listener to the submit button
+    submitButton.addEventListener('click', submitAnswer);
+  
+    // Function to submit the answer and load the next question
+    function submitAnswer() {
+      // Increment the question index
+      currentQuestionIndex++;
+  
+      // Check if all questions in the current category are answered
+      var category = categories[currentCategoryIndex];
+      if (currentQuestionIndex >= category.questions.length) {
+        // Check if there are more categories
+        if (currentCategoryIndex + 1 < categories.length) {
+          // Move to the next category
+          currentCategoryIndex++;
+          currentQuestionIndex = 0;
+        } else {
+          // End the quiz and display the result
+          endQuiz();
+          return;
+        }
+      }
+  
+      // Load the next question
+      loadQuestion();
+  
+      // Clear the selected option and enable option buttons
+      var optionButtons = optionsElement.querySelectorAll('.option-btn');
+      for (var i = 0; i < optionButtons.length; i++) {
+        optionButtons[i].disabled = false;
+        optionButtons[i].classList.remove('selected');
+      }
+  
+      // Hide the submit button
+      submitButton.style.display = 'none';
+    }
+  
+    // Function to end the quiz and display the result
+    function endQuiz() {
+      // Hide the question container
+      questionContainer.style.display = 'none';
+  
+      // Calculate the score percentage
+      var totalQuestions = 0;
+      var totalCorrect = 0;
+      for (var i = 0; i < categories.length; i++) {
+        totalQuestions += categories[i].questions.length;
+        totalCorrect += getCorrectAnswersCount(categories[i]);
+      }
+      var scorePercentage = (score / totalQuestions) * 100;
+  
+      // Display the result
+      resultElement.innerHTML = 'Quiz ended.<br>';
+      resultElement.innerHTML += 'Your score: ' + score + '/' + totalQuestions + '<br>';
+      resultElement.innerHTML += 'Score percentage: ' + scorePercentage.toFixed(2) + '%';
+    }
+  
+    // Function to count the number of correct answers in a category
+    function getCorrectAnswersCount(category) {
+      var correctCount = 0;
+      for (var i = 0; i < category.questions.length; i++) {
+        if (category.questions[i].answer === category.questions[i].selectedOption) {
+          correctCount++;
+        }
+      }
+      return correctCount;
+    }
+  
+    // Load the categories from the JSON file
+    fetch('data.json')
+      .then(response => response.json())
+      .then(data => {
+        categories = data.categories;
+      })
+      .catch(error => {
+        console.error('Error loading questions from data.json:', error);
+      });
+  });
+  
