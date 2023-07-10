d0579e02780357b32ea03227fa564086a3f59466
Danny Maggs <131584189+DMaggsy@users.noreply.github.com>
2023-07-10 15:40:31 +0100

    Update to script.js to handle firebase config instead of external file

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,40 @@ document.addEventListener('DOMContentLoaded', function () {
   var score = 0;
   var totalQuestions = 0;
 
+  const firebaseConfig = {
+    apiKey: "AIzaSyAEQRrArNsKOYMjeAVpYAWmaoK1e5HRYM0",
+    authDomain: "freshquiz-67c82.firebaseapp.com",
+    databaseURL: "https://freshquiz-67c82-default-rtdb.firebaseio.com",
+    projectId: "freshquiz-67c82",
+    storageBucket: "freshquiz-67c82.appspot.com",
+    messagingSenderId: "599950135432",
+    appId: "1:599950135432:web:cc30a733a8a661304b4f3d",
+    measurementId: "G-1444RBFMYN"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // Function to submit the answer and load the next question
+  function submitAnswer() {
+    // ...
+
+    // Create a new entry in the Firebase Realtime Database
+    var database = firebase.database();
+    var scoresRef = database.ref('quiz-scores');
+    scoresRef.push({
+      username: usernameInput.value,
+      score: score,
+      totalQuestions: totalQuestions,
+      scorePercentage: scorePercentage.toFixed(2)
+    });
+
+    // ...
+  }
+
+  // ...
+
+
   // Get the start button element
   var startButton = document.getElementById('start-btn');
 
@@ -170,7 +204,7 @@ document.addEventListener('DOMContentLoaded', function () {
         correctCount++;
       }
     }
-    
+
   }
 
   // Function to calculate the total number of questions in all categories
