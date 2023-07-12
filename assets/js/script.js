41f5b4570a624aa0050133b82b79b2426f89124b
Danny Maggs <131584189+DMaggsy@users.noreply.github.com>
2023-07-12 16:53:29 +0100

    Update structure to ensire loading of new questions from categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Define variables
   var usernameInput = document.getElementById('username-input');
+  // Define variables
   var categories;
   var currentCategoryIndex = 0;
   var currentQuestionIndex = 0;
@@ -37,8 +37,14 @@ document.addEventListener('DOMContentLoaded', function () {
   var enterUsernameButton = document.getElementById('enter-username');
   enterUsernameButton.addEventListener('click', enterUsername);
 
+  // Add event listener to the "Start quiz" button
+  var startQuizButton = document.getElementById('start-quiz');
+  startQuizButton.addEventListener('click', startQuiz);
+
   // Function to load the categories from the JSON file
   function loadCategories() {
+    console.log('Load categories function called');
+
     fetch('assets/data.json')
       .then(response => response.json())
       .then(data => {
@@ -72,6 +78,25 @@ document.addEventListener('DOMContentLoaded', function () {
       optionsElement.appendChild(optionButton);
     }
   }
+
+  // Get the question container elements
+  var questionContainer = document.getElementById('question-container');
+  var questionElement = document.getElementById('question');
+  var optionsElement = document.getElementById('options');
+
+  // Get the submit button and result elements
+  var submitButton = document.getElementById('submit-btn');
+  var resultElement = document.getElementById('result');
+
+  // Show the submit button
+  document.getElementById('submit-btn').style.display = 'block';
+
+  // Show the question container
+  document.getElementById('question-container').style.display = 'block';
+
+  // Add event listener to the submit button
+  submitButton.addEventListener('click', submitAnswer);
+
   // Function to handle option selection
   function selectOption(event) {
     var selectedOption = event.target;
@@ -87,102 +112,26 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add the "selected" class to the clicked option button
     selectedOption.classList.add('selected');
 
-    // Show the submit button
-    submitButton.style.display = 'block';
   }
-    // Function to submit the answer and load the next question
-    function submitAnswer() {
-      // Increment the question index
-      currentQuestionIndex++;
-  
-      // Check if all questions in the current category are answered
-      var category = categories[currentCategoryIndex];
-      if (currentQuestionIndex >= category.questions.length) {
-        // Check if there are more categories
-        if (currentCategoryIndex + 1 < categories.length) {
-          // Move to the next category
-          currentCategoryIndex++;
-          currentQuestionIndex = 0;
-        } else {
-          // End the quiz and display the result
-          endQuiz();
-          return;
-        }
-      }
-
-      
-  const firebaseConfig = {
-    apiKey: "AIzaSyAEQRrArNsKOYMjeAVpYAWmaoK1e5HRYM0",
-    authDomain: "freshquiz-67c82.firebaseapp.com",
-    databaseURL: "https://freshquiz-67c82-default-rtdb.firebaseio.com",
-    projectId: "freshquiz-67c82",
-    storageBucket: "freshquiz-67c82.appspot.com",
-    messagingSenderId: "599950135432",
-    appId: "1:599950135432:web:cc30a733a8a661304b4f3d",
-    measurementId: "G-1444RBFMYN"
-  };
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
   // Function to submit the answer and load the next question
   function submitAnswer() {
-    // ...
+    // Increment the question index
+    currentQuestionIndex++;
 
-    // Create a new entry in the Firebase Realtime Database
-    var database = firebase.database();
-    var scoresRef = database.ref('quiz-scores');
-    scoresRef.push({
-      username: usernameInput.value,
-      score: score,
-      totalQuestions: totalQuestions,
-      scorePercentage: scorePercentage.toFixed(2)
-    });
-
-    // ...
-  }
-
-  // ...
-
-
-  // Get the start button element
-  var startButton = document.getElementById('start-btn');
-
-  // Get the question container elements
-  var questionContainer = document.getElementById('question-container');
-  var questionElement = document.getElementById('question');
-  var optionsElement = document.getElementById('options');
-
-  // Get the submit button and result elements
-  var submitButton = document.getElementById('submit-btn');
-  var resultElement = document.getElementById('result');
-
-  // Get the end screen elements
-  var endScreen = document.getElementById('end-screen');
-  var submitScoreButton = document.getElementById('submit-score-btn');
-
-
-  // Function to load the categories from the JSON file
-  function loadCategories() {
-    console.log('Load categories function called');
-
-    fetch('assets/data.json')
-      .then(response => response.json())
-      .then(data => {
-        console.log('Data loaded:', data);
-
-        categories = data.categories;
-        totalQuestions = getTotalQuestionsCount();
-        // Load the first question
-        loadQuestion();
-      })
-      .catch(error => {
-        console.error('Error loading questions from data.json:', error);
-      });
-  }
-
-  // Add event listener to the submit button
-  submitButton.addEventListener('click', submitAnswer);
+    // Check if all questions in the current category are answered
+    var category = categories[currentCategoryIndex];
+    if (currentQuestionIndex >= category.questions.length) {
+      // Check if there are more categories
+      if (currentCategoryIndex + 1 < categories.length) {
+        // Move to the next category
+        currentCategoryIndex++;
+        currentQuestionIndex = 0;
+      } else {
+        // End the quiz and display the result
+        endQuiz();
+        return;
+      }
+    }
 
     // Load the next question
     loadQuestion();
@@ -193,9 +142,6 @@ document.addEventListener('DOMContentLoaded', function () {
       optionButtons[i].disabled = false;
       optionButtons[i].classList.remove('selected');
     }
-
-    // Hide the submit button
-    submitButton.style.display = 'none';
   }
 
   // Function to end the quiz and display the result
@@ -214,30 +160,6 @@ document.addEventListener('DOMContentLoaded', function () {
     resultElement.innerHTML += 'Your score: ' + score + '/' + totalQuestions + '<br>';
   }
 
-function submitScore() {
-  var username = usernameInput.value;
-  var scorePercentage = (score / totalQuestions) * 100;
-
-  // Display the result
-  resultElement.innerHTML += 'Username: ' + username;
-
-  // Clear the username input
-  usernameInput.value = '';
-}
-
-// Event listener for the submit score button
-submitScoreButton.addEventListener('click', function () {
-  var username = usernameInput.value;
-  var scorePercentage = (score / totalQuestions) * 100;
-
-  // Display the result
-  resultElement.innerHTML += 'Username: ' + username;
-
-  // Clear the username input
-  usernameInput.value = '';
-});
-
-
   // Function to count the number of correct answers in a category
   function getCorrectAnswersCount(category) {
     var correctCount = 0;
@@ -246,8 +168,9 @@ submitScoreButton.addEventListener('click', function () {
         correctCount++;
       }
     }
-
+    return correctCount;
   }
+
   // Function to calculate the total number of questions in all categories
   function getTotalQuestionsCount() {
     var totalQuestionsCount = 0;
@@ -257,87 +180,61 @@ submitScoreButton.addEventListener('click', function () {
     return totalQuestionsCount;
   }
 
-// Function to update the results element with the username and score
-function updateResults(score) {
-  var resultsElement = document.getElementById('results');
-  resultsElement.innerHTML = `
-    <h1>Your results</h1>
-    <p>Username: <strong>${usernameInput.value}</strong></p>
-    <p>Score: <strong>${score}</strong></p>
-  `;
-}
+  // Function to update the results element with the username and score
+  function updateResults(score) {
+    var resultsElement = document.getElementById('results');
+    resultsElement.innerHTML = `
+      <h1>Your results</h1>
+      <p>Username: <strong>${usernameInput.value}</strong></p>
+      <p>Score: <strong>${score}</strong></p>
+    `;
+  }
 
-// Event listener for the DOMContentLoaded event
-document.addEventListener('DOMContentLoaded', function () {
-  // Initialize the result element
-  var resultElement = document.getElementById('results');
-  var scoreInput = document.getElementById('score-input');
+  // Event listener for the submit score button
+  submitScoreButton.addEventListener('click', function () {
+    var username = usernameInput.value;
+    var scorePercentage = (score / totalQuestions) * 100;
 
-  // Hide the question container
-  questionContainer.style.display = 'none';
+    // Display the result
+    resultElement.innerHTML += 'Username: ' + username;
 
-  // Show the end screen
-  endScreen.style.display = 'none';
-
-  // Show the submit score button
-  submitScoreButton.style.display = 'none';
-});
-
-// Function to end the quiz and display the result
-function endQuiz() {
-  // Hide the question container
-  questionContainer.style.display = 'none';
-
-  // Show the end screen
-  endScreen.style.display = 'block';
-
-  // Show the submit score button
-  submitScoreButton.style.display = 'block';
-
-  // Call the updateResults function
-  updateResults(score);
-
-  // Display the result
-  resultElement.innerHTML = 'Quiz ended.<br>';
-  resultElement.innerHTML += 'Your score: ' + score + '/' + totalQuestions + '<br>';
-}
-
-
-
-
-
-// Event listener for the submit score button
-submitScoreButton.addEventListener('click', function () {
-  var username = usernameInput.value;
-  var scorePercentage = (score / totalQuestions) * 100;
-
-  // Display the result
-  resultElement.innerHTML += 'Username: ' + username;
-
-  // Clear the username input
-  usernameInput.value = '';
-});
-
-// Submit the username and score to the Firebase Realtime Database
-function submitScore() {
-  // Get the username and score from the user input
-  var username = usernameInput.value;
-  var score = score; // <-- Comment out this line
-
-  // Create a new entry in the Firebase Realtime Database
-  var database = firebase.database();
-  var scoresRef = database.ref('quiz-scores');
-  scoresRef.push({
-    username: username,
-    score: score
+    // Clear the username input
+    usernameInput.value = '';
   });
 
-  // Update the results element with the username and score
-  updateResults(score);
+  // Submit the username and score to the Firebase Realtime Database
+  function submitScore() {
+    // Get the username and score from the user input
+    var username = usernameInput.value;
+    var score = score; // <-- Comment out this line
 
-  // Clear the username and score inputs
-  usernameInput.value = '';
-  scoreInput.value = '';
-}
+    // Create a new entry in the Firebase Realtime Database
+    var database = firebase.database();
+    var scoresRef = database.ref('quiz-scores');
+    scoresRef.push({
+      username: username,
+      score: score
+    });
 
+    // Update the results element with the username and score
+    updateResults(score);
+
+    // Clear the username and score inputs
+    usernameInput.value = '';
+    scoreInput.value = '';
+  }
 });
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEQRrArNsKOYMjeAVpYAWmaoK1e5HRYM0",
+  authDomain: "freshquiz-67c82.firebaseapp.com",
+  databaseURL: "https://freshquiz-67c82-default-rtdb.firebaseio.com",
+  projectId: "freshquiz-67c82",
+  storageBucket: "freshquiz-67c82.appspot.com",
+  messagingSenderId: "599950135432",
+  appId: "1:599950135432:web:cc30a733a8a661304b4f3d",
+  measurementId: "G-1444RBFMYN"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
